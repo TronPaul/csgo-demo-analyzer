1100ce65513ec6dd445ecf01782ef9633f19b5d4
Mark McGuire <mark.b.mcg@gmail.com>
2016-02-15 16:28:08 -0600

    split out spec

--- src/main/clojure/csgo_demo_reader/core.clj
+++ src/main/clojure/csgo_demo_reader/core.clj
@@ -1,20 +1,9 @@
 (ns csgo-demo-reader.core
   (:require [clojure.java.io :as io]
             [octet.core :as buf]
-            [octet.spec :as spec]
-            [octet.spec.basic :as basic]
-            [octet.buffer :as buffer])
+            [csgo-demo-reader.spec :as spec])
   (:use [protobuf.core])
   (:import (com.valve NetmessagesPublic)))
-
-(defn read-int32LE [_ pos]
-  (Integer/reverseBytes (buffer/read-int _ pos)))
-
-(defn write-int32LE [_ pos value]
-  (buffer/write-int _ pos (Integer/reverseBytes value)))
-
-(def int32LE
-  (basic/primitive-spec 4 read-int32LE write-int32LE))
 
 (def bit-mask-table
   (concat [0]
@@ -23,9 +12,9 @@
            0xffffffff]))
 
 (defn next-word [input-stream]
-  (let [size-buf (buf/allocate (buf/size int32LE))]
+  (let [size-buf (buf/allocate (buf/size spec/int32LE))]
     (.read input-stream (.array size-buf))
-    (buf/read size-buf int32LE)))
+    (buf/read size-buf spec/int32LE)))
 
 (defn read-ubit-long [input-stream cur-bits num-bits]
   (if (>= (:size cur-bits) num-bits)
@@ -45,88 +34,19 @@
         new-result
         (recur (inc count) new-result new-bits)))))
 
-(defrecord Vector [x y z])
-
-(defn read-many [value-types buff pos]
-  (reduce (fn [coll value-type]
-            (let [prev-readed (first coll)
-                  cur-pos (+ pos (first coll))
-                  [readed ret] (spec/read value-type buff cur-pos)]
-              [(+ prev-readed readed) (conj (second coll) ret)]))
-          [0 []] value-types))
-
-(def vector-spec
-  (reify
-    spec/ISpecSize
-    (size [_]
-      (* 3 (spec/size buf/float)))
-
-    spec/ISpec
-    (read [_ buff pos]
-      (let [[readed vals] (read-many (repeat 3 (buf/float)) buff pos)]
-        [readed (apply ->Vector vals)]))
-
-    (write [_ buff pos vector])))
-
-(defrecord Split [flags
-                  view-origin
-                  view-angle
-                  local-view-angle
-                  resample-view-origin
-                  resample-view-angle
-                  resample-local-view-angle])
-
-(def split-spec
-  (reify
-    spec/ISpecSize
-    (size [_]
-      (+ (spec/size buf/int32) (* 6 (spec/size vector-spec))))
-
-    spec/ISpec
-    (read [_ buff pos]
-      (let [[readed vals] (read-many (cons buf/int32 (repeat 6 vector-spec)) buff pos)]
-        [readed (apply ->Split vals)]))
-    (write [_ buff pos vector])))
-
-(def demo-header
-  (buf/spec :filestamp  (buf/string 8)
-        :protocol int32LE
-        :network-protocol int32LE
-        :server-name (buf/string 260)
-        :client-name (buf/string 260)
-        :map-name (buf/string 260)
-        :game-directory (buf/string 260)
-        :playback-time buf/float
-        :playback-ticks int32LE
-        :playback-frames int32LE
-        :signon-length int32LE))
-
-(def cmd-header
-  (buf/spec :cmd buf/byte
-            :tick int32LE
-            :player_slot buf/byte))
-
-(def demo-cmd-info
-  (buf/spec :p1 split-spec
-            :p2 split-spec))
-
-(def sequence-info
-  (buf/spec :seq-in int32LE
-            :seq-out int32LE))
-
 (defn skip-raw-data [input-stream]
-  (let [size-buf (buf/allocate (buf/size int32LE))]
+  (let [size-buf (buf/allocate (buf/size spec/int32LE))]
     (.read input-stream (.array size-buf))
-    (let [size (long (buf/read size-buf int32LE))]
+    (let [size (long (buf/read size-buf spec/int32LE))]
       (loop [pos 0]
         (if (<= size pos)
           pos
           (recur (+ pos (.skip input-stream (- size pos)))))))))
 
 (defn read-raw-data [input-stream]
-  (let [size-buf (buf/allocate (buf/size int32LE))]
+  (let [size-buf (buf/allocate (buf/size spec/int32LE))]
     (.read input-stream (.array size-buf))
-    (let [size (buf/read size-buf int32LE)
+    (let [size (buf/read size-buf spec/int32LE)
           b (byte-array size)]
       (loop [pos 0]
         (if (<= size pos)
@@ -134,29 +54,29 @@
           (recur (+ pos (.read input-stream b pos (- size pos)))))))))
 
 (defn read-demo-header [input-stream]
-  (let [b (buf/allocate (buf/size demo-header))]
+  (let [b (buf/allocate (buf/size spec/demo-header))]
     (.read input-stream (.array b))
-    (buf/read b demo-header)))
+    (buf/read b spec/demo-header)))
 
 (defn read-cmd-header [input-stream]
-  (let [b (buf/allocate (buf/size cmd-header))]
+  (let [b (buf/allocate (buf/size spec/cmd-header))]
     (.read input-stream (.array b))
-    (buf/read b cmd-header)))
+    (buf/read b spec/cmd-header)))
 
 (defn read-demo-cmd-info [input-stream]
-  (let [b (buf/allocate (buf/size demo-cmd-info))]
+  (let [b (buf/allocate (buf/size spec/demo-cmd-info))]
     (.read input-stream (.array b))
-    (buf/read b demo-cmd-info)))
+    (buf/read b spec/demo-cmd-info)))
 
 (defn read-sequence-info [input-stream]
-  (let [b (buf/allocate (buf/size sequence-info))]
+  (let [b (buf/allocate (buf/size spec/sequence-info))]
     (.read input-stream (.array b))
-    (buf/read b sequence-info)))
+    (buf/read b spec/sequence-info)))
 
 (defn read-packet-size [input-stream]
-  (let [size-buf (buf/allocate (buf/size int32LE))]
+  (let [size-buf (buf/allocate (buf/size spec/int32LE))]
     (.read input-stream (.array size-buf))
-    (buf/read size-buf int32LE)))
+    (buf/read size-buf spec/int32LE)))
 
 (defn read-demo-packet [input-stream]
   (let [packet-size (read-packet-size input-stream)]
---
+++ src/main/clojure/csgo_demo_reader/spec.clj
@@ -0,0 +1,83 @@
+(ns csgo-demo-reader.spec
+  (:require [octet.core :as buf]
+            [octet.buffer :as buffer]
+            [octet.spec.basic :as basic]
+            [octet.spec :as spec]))
+
+(defn read-int32LE [_ pos]
+  (Integer/reverseBytes (buffer/read-int _ pos)))
+
+(defn write-int32LE [_ pos value]
+  (buffer/write-int _ pos (Integer/reverseBytes value)))
+
+(def int32LE
+  (basic/primitive-spec 4 read-int32LE write-int32LE))
+
+(defn read-many [value-types buff pos]
+  (reduce (fn [coll value-type]
+            (let [prev-readed (first coll)
+                  cur-pos (+ pos (first coll))
+                  [readed ret] (spec/read value-type buff cur-pos)]
+              [(+ prev-readed readed) (conj (second coll) ret)]))
+          [0 []] value-types))
+
+(defrecord Vector [x y z])
+
+(def cs-vector
+  (reify
+    spec/ISpecSize
+    (size [_]
+      (* 3 (spec/size buf/float)))
+
+    spec/ISpec
+    (read [_ buff pos]
+      (let [[readed vals] (read-many (repeat 3 (buf/float)) buff pos)]
+        [readed (apply ->Vector vals)]))
+
+    (write [_ buff pos vector])))
+
+(defrecord Split [flags
+                  view-origin
+                  view-angle
+                  local-view-angle
+                  resample-view-origin
+                  resample-view-angle
+                  resample-local-view-angle])
+
+(def split
+  (reify
+    spec/ISpecSize
+    (size [_]
+      (+ (spec/size buf/int32) (* 6 (spec/size cs-vector))))
+
+    spec/ISpec
+    (read [_ buff pos]
+      (let [[readed vals] (read-many (cons buf/int32 (repeat 6 cs-vector)) buff pos)]
+        [readed (apply ->Split vals)]))
+    (write [_ buff pos vector])))
+
+(def demo-header
+  (buf/spec :filestamp  (buf/string 8)
+            :protocol int32LE
+            :network-protocol int32LE
+            :server-name (buf/string 260)
+            :client-name (buf/string 260)
+            :map-name (buf/string 260)
+            :game-directory (buf/string 260)
+            :playback-time buf/float
+            :playback-ticks int32LE
+            :playback-frames int32LE
+            :signon-length int32LE))
+
+(def cmd-header
+  (buf/spec :cmd buf/byte
+            :tick int32LE
+            :player_slot buf/byte))
+
+(def demo-cmd-info
+  (buf/spec :p1 split
+            :p2 split))
+
+(def sequence-info
+  (buf/spec :seq-in int32LE
+            :seq-out int32LE))
