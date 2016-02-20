2abfe2f4e299cedb660f9a930cf5e5d34c80c90e
Mark McGuire <mark.b.mcg@gmail.com>
2016-02-20 12:53:10 -0600

    add comment for bad acting library

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-protobuf "0.4.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.google.protobuf/protobuf-java "2.5.0"]
-                 [org.flatland/protobuf "0.8.1"]
+                 [org.flatland/protobuf "0.8.1"]            ; THIS INCLUDES protobuf-java SOURCE for 2.5.0 (why? no idea).
+                                                            ; DON'T CHANGE THE VERSION.
                  [funcool/octet "0.2.0"]]
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"])
